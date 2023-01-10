ca88b5a02ab6dc575b7b435c0f6268463af0954f
Matthias <tuttiacc@hotmail.com>
2023-01-10 15:34:46 +0100

    unminimize not working repro

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::{Menu, WindowBuilder, WindowUrl, Submenu, CustomMenuItem};
+use tauri::{Menu, WindowBuilder, WindowUrl, Submenu, CustomMenuItem, Manager};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -19,13 +19,22 @@ fn open_window(app_handle: tauri::AppHandle) -> () {
     ))).build();
 }
 
+#[tauri::command]
+fn unminimize_window(app_handle: tauri::AppHandle) -> () {
+    println!("Attempting to unminimize window.");
+    match app_handle.get_window("external_window") {
+        Some(window) => window.unminimize().unwrap(),
+        None => println!("@unminimize_window: External window is not open."),
+    };
+}
+
 fn main() {
     tauri::Builder::default()
         .menu(Menu::new().add_submenu(Submenu::new(
             "Main Window Option",
             Menu::new().add_item(CustomMenuItem::new("Hello-from-main", "Hello From Main")),
         )))
-        .invoke_handler(tauri::generate_handler![open_window])
+        .invoke_handler(tauri::generate_handler![open_window, unminimize_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -8,8 +8,19 @@ async function openExternal() {
   await invoke("open_window", { });
 }
 
+
+async function unminimizeExternal() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  console.log("Unminimizing external window.");
+  await invoke("unminimize_window", { });
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector("#open-external-button")
     .addEventListener("click", () => openExternal());
+
+  document
+    .querySelector("#unminimize-external-button")
+    .addEventListener("click", () => unminimizeExternal());
 });
